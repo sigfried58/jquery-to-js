ef125a40e432b9f36382c5c4251e425f1108e2cb
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-10-16 00:15:12 -0300

    Creation of elements and assignment of attributes

--- src/js/home.js
+++ src/js/home.js
@@ -73,10 +73,24 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   }
   const $form = document.getElementById('form');
   const $home = document.getElementById('home');
+  const $featuringContainer = document.getElementById('featuring');
+
+  function setAttributes($element, attributes) {
+    for (const attribute in attributes) {
+      $element.setAttribute(attribute, attributes[attribute]);
+    }
+  }
 
   $form.addEventListener('submit', event => {
     event.preventDefault();
     $home.classList.add('search-active');
+    const $loader = document.createElement('img');
+    setAttributes($loader, {
+      src: 'src/images/loader.gif',
+      height: 50,
+      width: 50
+    });
+    $featuringContainer.append($loader);
   });
 
   const actionList = await getData(
@@ -128,8 +142,6 @@ fetch('https://randomuser.me/api/dsfdsfsd')
 
   const $animationContainer = document.getElementById('animation');
   renderMovieList(animationList.data.movies, $animationContainer);
-
-  const $featuringContainer = document.getElementById('#featuring');
 
   // const $home = $('.home .list #item');
   const $modal = document.getElementById('modal');
