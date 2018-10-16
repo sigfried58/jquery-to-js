1923adccadd89c341c81d5ee1b87aec31f3d3923
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-10-16 13:14:37 -0300

    Finding items in the list

--- src/js/home.js
+++ src/js/home.js
@@ -118,11 +118,15 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     $featuringContainer.innerHTML = HTMLString;
   });
 
-  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
-  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`);
-  const animationList = await getData(
-    `${BASE_API}list_movies.json?genre=animation`
-  );
+  const {
+    data: { movies: actionList }
+  } = await getData(`${BASE_API}list_movies.json?genre=action`);
+  const {
+    data: { movies: dramaList }
+  } = await getData(`${BASE_API}list_movies.json?genre=drama`);
+  const {
+    data: { movies: animationList }
+  } = await getData(`${BASE_API}list_movies.json?genre=animation`);
   console.log(actionList, dramaList, animationList);
   function videoItemTemplate(movie, category) {
     return `<div class="primaryPlaylistItem" data-id="${
@@ -158,13 +162,13 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     });
   }
   const $actionContainer = document.querySelector('#action');
-  renderMovieList(actionList.data.movies, $actionContainer, 'action');
+  renderMovieList(actionList, $actionContainer, 'action');
 
   const $dramaContainer = document.getElementById('drama');
-  renderMovieList(dramaList.data.movies, $dramaContainer, 'drama');
+  renderMovieList(dramaList, $dramaContainer, 'drama');
 
   const $animationContainer = document.getElementById('animation');
-  renderMovieList(animationList.data.movies, $animationContainer, 'animation');
+  renderMovieList(animationList, $animationContainer, 'animation');
 
   // const $home = $('.home .list #item');
   const $modal = document.getElementById('modal');
@@ -175,11 +179,34 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   const $modalImage = $modal.querySelector('img');
   const $modalDescription = $modal.querySelector('p');
 
+  function findById(list, id) {
+    return list.find(movie => movie.id === parseInt(id, 10));
+  }
+
+  function findMovie(id, category) {
+    switch (category) {
+      case 'action': {
+        return findById(actionList, id);
+      }
+      case 'drama': {
+        return findById(dramaList, id);
+      }
+      default: {
+        return findById(animationList, id);
+      }
+    }
+  }
+
   function showModal($element) {
     $overlay.classList.add('active');
     $modal.style.animation = 'modalIn .8s forwards';
     const id = $element.dataset.id;
     const category = $element.dataset.category;
+    const data = findMovie(id, category);
+
+    $modalTitle.textContent = data.title;
+    $modalImage.setAttribute('src', data.medium_cover_image);
+    $modalDescription.textContent = data.description_full;
   }
 
   $hideModal.addEventListener('click', hideModal);
